ad8b17cde8834cca2036810ad0d5f21b8c12e382
Kodo Senpai <64303016+kodosenpai@users.noreply.github.com>
2020-08-10 20:40:48 -0700

    comment out unused code to prevent ci failures

--- src/components/Kitchen.jsx
+++ src/components/Kitchen.jsx
@@ -2,30 +2,30 @@ import React from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
-import styled from 'styled-components';
+// import styled from 'styled-components';
 
-const Salmon = styled.div`
-  width: 20px;
-  height: 60px;
-  background: #f45b69;
-  border-radius: 6px;
-  background-image: linear-gradient(rgba(255, 255, 255, 0.3) 10%, #f45b69 10%);
-  background-size: 10px 10px;
-`;
+// const Salmon = styled.div`
+//   width: 20px;
+//   height: 60px;
+//   background: #f45b69;
+//   border-radius: 6px;
+//   background-image: linear-gradient(rgba(255, 255, 255, 0.3) 10%, #f45b69 10%);
+//   background-size: 10px 10px;
+// `;
 
-const Tuna = styled.div`
-  width: 20px;
-  height: 60px;
-  background: #8b0000;
-  border-radius: 6px;
-  background-image: linear-gradient(rgba(255, 255, 255, 0.3) 10%, #8b0000 10%);
-  background-size: 10px 10px;
-`;
+// const Tuna = styled.div`
+//   width: 20px;
+//   height: 60px;
+//   background: #8b0000;
+//   border-radius: 6px;
+//   background-image: linear-gradient(rgba(255, 255, 255, 0.3) 10%, #8b0000 10%);
+//   background-size: 10px 10px;
+// `;
 
-const ingredientMap = {
-  Salmon,
-  Tuna,
-};
+// const ingredientMap = {
+//   Salmon,
+//   Tuna,
+// };
 
 export const Kitchen = ({
   ingredients,
